e9d8c35e122f2302101695f4212f48a52b69c639
Anthony Braun <antmbraun@gmail.com>
2024-12-03 20:21:40 -0500

    day2 part 2

---
+++ day2/day2.2.py
@@ -0,0 +1,42 @@
+def solution():
+    # Read src.txt
+    source = open("src.txt", "r")
+    lines = source.read().split("\n")
+    source.close()
+
+    result = 0
+
+    for line in lines:
+        report = line.split(" ")
+        if isReportSafe(report):
+            result += 1
+
+    return result
+
+def isReportSafe(report, is_damper_check = False):
+    
+    prev_diff = 0
+    flagReport = False
+
+    for i in range(1, len(report)):
+        curr_diff = int(report[i]) - int(report[i - 1])
+
+        # Check that difference is within range
+        if not 1 <= abs(curr_diff) <= 3:
+            flagReport = True
+
+        # Check for change in direction
+        if curr_diff * prev_diff < 0:
+            flagReport = True
+
+        if flagReport:
+          if not is_damper_check:
+              return isReportSafe(report[:i] + report[i+1:], True) or isReportSafe(report[:i-1] + report[i:], True)
+          else:
+              return False
+          
+        prev_diff = curr_diff
+
+    return True
+
+print(solution())
