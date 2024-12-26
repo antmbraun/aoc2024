5a34c2a133d8dc63f76d44dac49f36e73213cb90
Anthony Braun <antmbraun@gmail.com>
2024-12-26 16:21:30 -0500

    day 10

---
+++ day10/day10.py
@@ -0,0 +1,43 @@
+def solution():
+    # Read src.txt
+    source = open("src.txt", "r")
+    input = source.read().split('\n')
+    source.close()
+    
+    directions = [
+        [-1, 0],
+        [0, 1],
+        [1, 0],
+        [0, -1]
+    ]
+
+    def dfs(r, c):
+        if input[r][c] == '9' and (r, c) not in found:
+            found.add((r, c))
+            return 1
+        
+        count = 0
+
+        for dir in directions:
+            r_new = r + dir[0]
+            c_new = c + dir[1]
+            if (
+              r_new < len(input) and
+              r_new >= 0 and
+              c_new < len(input[0]) and
+              c_new >= 0 and
+              int(input[r_new][c_new]) - int(input[r][c]) == 1):
+                  count += dfs(r_new, c_new)
+        return count
+    
+    result = 0
+    for r, row in enumerate(input):
+        for c, num in enumerate(row):
+            if num == '0':
+                found = set()
+                result += dfs(r, c)
+
+    return result
+
+
+print(solution())
